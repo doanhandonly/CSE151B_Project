ba9b4ee2f2fe5e6137ecaa152e59181fb6e7b1f0
doanhandonly <doanhnguyen7204@gmail.com>
2025-03-17 01:10:57 -0700

    added swa and warmup

---
+++ main.py
@@ -0,0 +1,174 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from model import UNet, CustomModel
+from dataloader import SegmentationDataset
+from util import compute_mean_std
+import torchvision.transforms as transforms
+import torchvision
+from tqdm import tqdm
+
+from transformers import get_linear_schedule_with_warmup
+
+
+
+ARCHIVE_ROOT = "archive"
+MASK_ROOT = "masks"
+BATCH_SIZE = 8
+LR = 0.001
+EPOCHS = 10
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#mean, std = compute_mean_std(ARCHIVE_ROOT, split="train")
+#print(mean, std)
+image_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.49145114, 0.46978662, 0.42543335], std=[0.15824795, 0.16471928, 0.17677158])
+])
+
+mask_transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+train_dataset = SegmentationDataset("train", transform=image_transform, target_transform=mask_transform, augment=True)
+val_dataset = SegmentationDataset("val", transform=image_transform, target_transform=mask_transform, augment=True)
+test_dataset = SegmentationDataset("test", transform=image_transform, target_transform=mask_transform, augment=True)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+model = UNet().to(DEVICE)
+criterion = nn.BCEWithLogitsLoss()
+class_criterion = nn.CrossEntropyLoss()
+optimizer = optim.AdamW(model.parameters(), lr=LR)
+
+def train():
+    best_val_loss = float('inf') 
+
+    model.train()
+    for epoch in range(EPOCHS):
+        total_loss = 0
+        loop = tqdm(train_loader, leave=True, desc=f"Epoch {epoch+1}/{EPOCHS}")
+
+        for images, masks, labels in loop:
+            images, masks, labels = images.to(DEVICE), masks.to(DEVICE), labels.to(DEVICE)
+
+            seg_output, class_output = model(images)  
+
+            loss_seg = criterion(seg_output, masks)  
+            loss_class = class_criterion(class_output, labels)  
+
+            loss = loss_seg + loss_class  
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {total_loss / len(train_loader):.4f}")
+        val_loss = validate()
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+
+            torch.save(model.state_dict(), 'best_model.pth')
+            print(f"Best model saved with validation loss: {best_val_loss:.4f}")
+
+def custom_train():
+    model = CustomModel().to(DEVICE)
+    best_val_loss = float('inf') 
+    
+    num_training_steps = len(train_loader) * EPOCHS
+    num_warmup_steps = int(0.1 * num_training_steps)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps = num_warmup_steps,
+        num_training_steps = num_training_steps
+    )
+
+    swa_scheduler = model.get_swa_scheduler(optimizer)
+
+    model.train()
+    for epoch in range(EPOCHS):
+        total_loss = 0
+        loop = tqdm(train_loader, leave=True, desc=f"Epoch {epoch+1}/{EPOCHS}")
+
+        for images, masks, labels in loop:
+            images, masks, labels = images.to(DEVICE), masks.to(DEVICE), labels.to(DEVICE)
+
+            seg_output, class_output = model(images)  
+
+            loss_seg = criterion(seg_output, masks)  
+            loss_class = class_criterion(class_output, labels)  
+
+            loss = loss_seg + loss_class  
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+        if epoch >= model.swa_start:
+            if not model.swa_active:
+                model.swa_active = True  
+            model.update_swa()  
+            swa_scheduler.step() 
+        else:
+            scheduler.step()  
+
+        print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {total_loss / len(train_loader):.4f}")
+        val_loss = validate()
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+
+            torch.save(model.state_dict(), 'best_model.pth')
+            print(f"Best model saved with validation loss: {best_val_loss:.4f}")
+
+    model.swap_swa_weights()
+    model.update_bn(train_loader, DEVICE)
+
+def validate():
+    model.eval()
+    total_loss = 0
+    loop = tqdm(val_loader, leave=True, desc="Validating")
+
+    with torch.no_grad():
+        for images, masks, labels in loop:
+            images, masks, labels = images.to(DEVICE), masks.to(DEVICE), labels.to(DEVICE)
+
+            seg_output, class_output = model(images)
+            loss_seg = criterion(seg_output, masks)
+            loss_class = class_criterion(class_output, labels)
+            loss = loss_class + loss_seg
+            total_loss += loss.item()
+
+    print(f"Validation Loss: {total_loss / len(val_loader):.4f}")
+    return total_loss / len(val_loader)
+
+def test():
+    model.eval()
+
+    model.load_state_dict(torch.load('best_model.pth'))
+    print("Best model loaded for testing.")
+    with torch.no_grad():
+        for i, (image, mask, label) in enumerate(test_loader):
+            print(i)
+            image, mask, label = image.to(DEVICE), mask.to(DEVICE), label.to(DEVICE)
+            output, class_output = model(image)  
+
+            predicted_class = torch.argmax(class_output, dim=1).item()
+            predicted_letter = "Blank" if predicted_class == 26 else chr(predicted_class + ord('A'))
+
+            torchvision.utils.save_image(image, f"results/input_{i}.png")
+            torchvision.utils.save_image(mask, f"results/mask_{i}.png")
+            torchvision.utils.save_image(output, f"results/output_{i}.png")
+
+            print(f"Image {i}: Predicted Letter - {predicted_letter}")
+
+if __name__ == "__main__":
+    train()
+    test()
---
+++ model.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.optim import swa_utils
+
+from config import load_config
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+config = load_config('configs.yaml')
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, num_classes=27):
+        super(UNet, self).__init__()
+        
+        self.encoder1 = self.conv_block(in_channels, 64)
+
+        self.encoder2 = self.conv_block(64, 128)
+        self.encoder3 = self.conv_block(128, 256)
+        
+        self.bottleneck = self.conv_block(256, 512) 
+
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.decoder3 = self.conv_block(512, 256)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.decoder2 = self.conv_block(256, 128)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.decoder1 = self.conv_block(128, 64)
+        
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)), 
+            nn.Flatten(),
+            nn.Linear(64, num_classes)
+        )
+        self.pool = nn.MaxPool2d(2)
+    
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        e1 = self.encoder1(x)
+        p1 = self.pool(e1)
+        e2 = self.encoder2(p1)
+        p2 = self.pool(e2)
+        e3 = self.encoder3(p2)
+        p3 = self.pool(e3)
+
+        b = self.bottleneck(p3)
+
+        d3 = self.upconv3(b)
+        d3 = crop_tensor(d3, e3)
+        d3 = torch.cat([d3, e3], dim=1)
+        d3 = self.decoder3(d3)
+
+        d2 = self.upconv2(d3)
+        d2 = crop_tensor(d2, e2)
+        d2 = torch.cat([d2, e2], dim=1)
+        d2 = self.decoder2(d2)
+
+        d1 = self.upconv1(d2)
+        d1 = crop_tensor(d1, e1)
+        d1 = torch.cat([d1, e1], dim=1)
+        d1 = self.decoder1(d1)
+
+        class_output = self.classifier(d1)
+        output = self.final_conv(d1)
+        return output, class_output
+    
+def crop_tensor(target, reference):
+    _, _, h1, w1 = target.shape
+    _, _, h2, w2 = reference.shape
+    diff_h = h1 - h2
+    diff_w = w1 - w2
+    target = target[:, :, :h1 - diff_h, :w1 - diff_w]
+    return target
+
+class CustomModel(UNet):
+    def __init__(self, in_channels=3, out_channels=1, num_classes=27):
+        super().__init__(in_channels, out_channels, num_classes)
+
+        self.swa_model = swa_utils.AveragedModel(self).to(device)
+        self.swa_active = False 
+        self.swa_start = int(config['epochs'] * .75)
+        self.swa_lr = 1e-4 
+
+    def update_swa(self):
+        self.swa_model.update_parameters(self)
+
+    def swap_swa_weights(self):
+        for param, swa_param in zip(self.parameters(), self.swa_model.parameters()):
+            param.data = swa_param.data.clone()
+
+    def get_swa_scheduler(self, optimizer):
+        return swa_utils.SWALR(optimizer, swa_lr = self.swa_lr, anneal_epochs = 5)
+
+    def update_bn(self, train_loader, device):
+        swa_utils.update_bn(train_loader, self.swa_model, device = device)
+    
